66dab66d67dacaf0ee0c5085b808d4bd2221f44a
Akarizwei <andy.b.sky@yandex.ru>
2019-10-13 01:49:33 +0400

    feat: add GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,31 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.l = null;
+        this.r = null;
+        this.previous = null;
+        this.middle = null;
+        this.range = [];
+    }
 
     setRange(min, max) {
-
+        for (let i = min; i <= max; i++) {
+            this.range.push(i);
+        }
+        this.l = 0;
+        this.r = this.range.length - 1;
     }
 
     guess() {
-
+        this.middle = Math.ceil((this.l + this.r) / 2);
+        return this.range[this.middle];
     }
 
     lower() {
-
+        this.r = this.middle;
     }
 
     greater() {
-
+        this.l = this.middle;
     }
 }
 
